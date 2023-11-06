e4bfe364642a3f739c821761c23c69c3f68a714e
Frederic Hodges <frederichodges@gmail.com>
2023-11-06 16:58:08 -0600

    Basic board UI

--- resources/js/components/BoardComponent.vue
+++ resources/js/components/BoardComponent.vue
@@ -1,7 +1,86 @@
 <template>
-    <div>
-        <h1>Board component</h1>
-    </div>
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="row d-flex flex-nowrap">
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+            </div>
+            <div class="row d-flex flex-nowrap">
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+                <div class="square dark"></div>
+                <div class="square light"></div>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -9,3 +88,19 @@
         name: 'BoardComponent',
     }
 </script>
+
+<style scoped>
+    .square {
+        height: 100px;
+        width: 100px;
+        padding: 0;
+    }
+
+    .square.light {
+        background-color: #b5b4b3;
+    }
+
+    .square.dark {
+        background-color: #70706f;
+    }
+</style>
