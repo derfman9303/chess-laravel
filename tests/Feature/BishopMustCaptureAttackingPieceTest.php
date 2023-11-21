b5f066cc09eb61337d2efb043b4781e8f3133f28
Frederic Hodges <frederichodges@gmail.com>
2023-11-21 14:22:12 -0600

    Add BishopMustCaptureAttackingPieceTest

---
+++ tests/Feature/BishopMustCaptureAttackingPieceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BishopMustCaptureAttackingPieceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_bishop_must_capture_attacking_piece()
+    {
+        $board = $this->newBoard();
+
+        $pieces = [
+            $this->newPiece('king', 'black', 4, 4),
+            $this->newPiece('king', 'white', 7, 4),
+            $this->newPiece('queen', 'white', 0, 5),
+            $this->newPiece('rook', 'white', 4, 1),
+            $this->newPiece('bishop', 'black', 2, 3),
+        ];
+
+        $this->indexPieces($pieces);
+        $this->setKingMoved($pieces);
+        $this->loadBoard($board, $pieces);
+
+        $response = $this->post('/get-move', [
+            'board'  => $board,
+            'pieces' => $pieces,
+            'turn'   => false,
+            'steps'  => 3,
+        ]);
+
+        $this->assertEquals([4, 4, 1], $response->json());
+    }
+}
